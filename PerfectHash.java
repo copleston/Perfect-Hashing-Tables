09bccace43450cce7c0fdfc8f17e13e3ae7a1120
Oliver <o.copleston@gmail.com>
2017-02-28 12:18:23 +0000

    Completed outer hash table

--- PerfectHash.java
+++ PerfectHash.java
@@ -6,35 +6,57 @@ import static java.lang.System.out;
 import java.util.*;
 
 class PerfectHash {
-    // Replace with ArrayList
-    // private int[] keys;
-    // private int[] values;
-    private Vector<Integer> keys;
-    private Vector values;
+    int p = 40487, a = 13, b = 1207;
+    int hashSize, col, cPairs;
+    int[] count;
+    private Vector<Integer> keysIn, outerHash;
+    private Vector<Vector<Integer>> outerTable, innerTable;
+    private Scanner sc;
 
-    private PerfectHash(int length) {
-        if (length < 0)
-          throw new IllegalArgumentException
-            ("The initialSize is negative: " + length);
+    private PerfectHash(String fileName) {
+        readFile(fileName);
+        System.out.format("INITIAL OUTER HASH FUNCTION PARAMETERS: a = %d; b = %d; p = %d\n", a, b, p);
+        findOuterHash();
+        findGroupings();
 
-        System.out.println("SETTING HASH TABLE SIZE: " + length);
+        printCount();
 
-        // keys = new Vector(length);
-        // values = new Vector(length);
-
-        int[] keyAr = new int[length];
-        Arrays.fill(keyAr, -1);
-        System.out.println("Size: " + keyAr.length);
-        // Arrays.fill(values, -1);
-
-        // VECTOR:
-        // create arrays of -1
-        // copy values of arrays into the vectors
-        keys = new Vector<Integer>(Arrays.asList(keyAr));
+        createInnerTable();
     }
 
-    private static void printArray(Enumeration<Integer> inArray) {
-        for (Integer i: inArray) System.out.format("%s ", i.toString());;
+    private void readFile(String name) {
+        // Initialise the file and scanner objects
+        File file = new File(name);
+
+        try {
+            sc = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.err.println("Error reading file: " + e);
+        }
+        // set size of hash table, then advance the scanner onto next line
+        hashSize = sc.nextInt();
+
+        if (hashSize < 0)
+          throw new IllegalArgumentException
+            ("The initialSize is negative: " + hashSize);
+
+        System.out.println("SETTING HASH TABLE SIZE: " + hashSize);
+
+        keysIn = new Vector<Integer>(); // Must specify Integer type to avoid compilaiton warnings
+        while (sc.hasNext()) {
+            keysIn.add(sc.nextInt());
+        }
+        sc.close();
+
+        System.out.print("READ SET OF KEYS: ");
+        printArray(keysIn);
+    }
+
+    private static void printArray(Vector inArray) {
+        // for (Integer i: inArray.iterator()) System.out.format("%s ", i.toString());
+        for (int i = 0; i < inArray.size(); i++) {
+            System.out.format("%s ", inArray.get(i));
+        }
         System.out.println();
     }
 
@@ -51,25 +73,102 @@ class PerfectHash {
         return 1;
     }
 
-    private static int hash(int k) {
-        // ((a*k + b) % p) % m
-        // p is prime
-        // k < p
-        // a >> {1 ... p-1}
-        // b >> {0 ... p-1}
-        return 1;
+    private int hash(int k) {
+        return ((a*k + b) % p) % hashSize;
+    }
+
+    private void increment() {
+        a = (a + 1) % p;
+        if (a == 0) a++;
+        b  = (b + 177) % p;
+    }
+
+    private void outerHash(int m) {
+        col = 1;
+        count = new int[m];
+        outerHash = new Vector <Integer>();
+
+        for (Integer i : keysIn) {
+            int h = hash(i);
+            outerHash.add(h);
+            count[h]++;
+            if (count[h] > 1) col++;
+        }
+        cPairs = ((col*col)-col)/2;
+    }
+
+    private void findOuterHash() {
+        int count = 0;
+
+        System.out.print("HASHED TO OUTER HASH TABLE AT: ");
+        outerHash(hashSize);
+        printArray(outerHash);
+
+        do {
+            if (count > 0) increment();
+            outerHash(hashSize);
+            System.out.format("\nNUMBER OF PAIRS OF COLLISIONS IN OUTER HASH TABLE: %d", cPairs);
+            count++;
+        } while (cPairs > hashSize);
+
+        System.out.format("\n%d OUTER HASH FUNCTIONS TESTED\n", count);
+
+        if (count > 1) {
+            System.out.format("MODIFIED OUTER HASH FUNCTION PARAMETERS: a = %d; b = %d; p = %d\n", a, b, p);
+            System.out.print("MODIFIED HASHING TO OUTER HASH TABLE AT: ");
+            printArray(outerHash);
+        }
+    }
+
+    private void printGroupings() {
+        System.out.println("\nKEYS GROUPED ONTO SLOTS:");
+        for (int i=0; i < hashSize; i++) {
+            System.out.format("grouping slot  %d:  ", i);
+            printArray(outerTable.get(i));
+        }
+    }
+
+    private void printInnerTable() {
+        for (int i=0; i < hashSize; i++) {
+            System.out.format("operation slot  %d:  ", i);
+            printArray(innerTable.get(i));
+        }
+    }
+
+    private void findGroupings() {
+        // Initialise the outer hashtable: A collection of Vector objects
+        outerTable = new Vector<Vector<Integer>>(hashSize);
+        for (int i=0; i < hashSize; i++) {
+            outerTable.add(i, new Vector<Integer>());
+        }
+
+        int index = 0;
+        for (Integer i : keysIn) {                          // For each item in the input...
+            outerTable.get( outerHash.get(index) ).add(i);  // Add to it's respective index, given in hte outerHash Vector
+            index++;
+        }
+        printGroupings(); // Print the groupings of the outer table
+    }
+
+    private void printCount() {
+        System.out.print("\nCollisions at: ");
+        for (int i=0; i < hashSize; i++) {
+            System.out.format("%d ", count[i]);
+        }
+        System.out.print("\n\n");
+    }
+
+    private void createInnerTable() {
+        innerTable = new Vector<Vector<Integer>>(hashSize);
+        for (int i=0; i < hashSize; i++) {
+            Integer[] keyAr = new Integer[count[i]*count[i]];
+            Arrays.fill(keyAr, -1);
+            innerTable.add(i, new Vector <Integer>(Arrays.asList(keyAr)));
+        }
+        printInnerTable();
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-        // Initialise the file and scanner objects
-        File file = new File("data.txt");
-        Scanner sc = new Scanner(file);
-
-        // set size of hash table, then advance the scanner onto next line
-        int hashSize = sc.nextInt();
-        PerfectHash p = new PerfectHash(hashSize);
-
-        printArray(p.keys.elements());
-        // p.keys.elements()
+        PerfectHash p = new PerfectHash("data2.txt"); // allows to read file
     }
 }
